5401bedd57f05db25495ac98e5a46564df2d0a32
Ottavio42 <ottaviog88@gmail.com>
2021-10-07 17:21:30 +0200

    card-e-films

---
+++ src/components/Films.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+	<h2>I Film</h2>
+	<ul>
+		<li v-for="(film, index) in films" :key="index">
+			<Card :details="film"/>
+		</li>
+	</ul>
+	<h2>Le Serie Tv</h2>
+	<ul>
+		<li v-for="(serieTv, index) in serie" :key="index">
+			<Card :details="serieTv"/>
+		</li>
+	</ul>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue';
+export default {
+	name: "Films",
+	props: ['films', 'serie'],
+	components: {
+		Card
+	}
+}
+</script>
+
+<style>
+</style>
