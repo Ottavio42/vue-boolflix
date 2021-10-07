8052dfedca3e52053dd10b5e2a5bbeca8e050ef8
Ottavio42 <ottaviog88@gmail.com>
2021-10-07 17:21:11 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<header>
+		<form>
+			<input type="text" v-model="searchText">
+			<button type="submit" @click.prevent="$emit('searching', searchText)">Cerca</button>
+		</form>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	data() {
+		return {
+			searchText: ""
+		}
+	}
+}
+</script>
+
+<style>
+</style>
